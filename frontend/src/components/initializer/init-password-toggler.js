92327cfa49c7a594e741ebd1c6de4d52b665e552
Юрий <nathanielkatesimon@gmail.com>
2026-02-11 23:38:48 +0800

    baseline registration UI
    - Intended as reusable base or reference for other registration flows
    - No backend integration yet

---
+++ frontend/src/components/initializer/init-password-toggler.js
@@ -0,0 +1,10 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function InitPasswordToggler() {
+  useEffect(() => {
+    window.initPasswordToggler()
+  }, []);
+  return null;
+}
