16ac7471af02ba8c9d3374763366d14aca3c2ff2
Юрий <nathanielkatesimon@gmail.com>
2026-02-13 12:08:43 +0800

    Add hyperlinks to homepage

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="p-12">
+      <Link href="/staff/login" className="text-info d-block fs-4 mb-12">Staff Login</Link>
+      <Link href="/staff/register" className="text-info d-block fs-4 mb-12">Staff Register</Link>
+      <Link href="/student/login" className="text-info d-block fs-4 mb-12">Student Login</Link>
+      <Link href="/student/register" className="text-info d-block fs-4 mb-12">Student Register</Link>
     </div>
   );
 }
