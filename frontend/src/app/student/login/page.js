0e5c2943a0b56a43f344e18bb124705e8dfc8888
Юрий <nathanielkatesimon@gmail.com>
2026-02-17 17:53:12 +0800

    Student login

--- frontend/src/app/student/login/page.js
+++ frontend/src/app/student/login/page.js
@@ -4,6 +4,7 @@ import CopyrightNotice from "@/components/ui/copyright-notice";
 import InitPasswordToggler from "@/components/initializer/init-password-toggler";
 import GuestOnlyGuard from "@/components/features/auth/guest-only-guard";
 import Link from "next/link";
+import StudentLoginForm from "./components/form";
 
 export default function StudentLogin() {
   return (
@@ -14,54 +15,9 @@ export default function StudentLogin() {
           <Link href="/staff/login" className="btn btn-info rounded-pill fs-5">Staff login</Link>
         </div>
         <AuthCard>
-          <form
-            id="formAuthentication"
-            className="mb-6 fv-plugins-bootstrap5 fv-plugins-framework px-8"
-            action="index.html"
-            method="GET"
-            noValidate
-          >
-            <div className="mb-6 fv-plugins-icon-container">
-              <input
-                type="text"
-                className="form-control form-control-lg"
-                id="email"
-                name="email"
-                placeholder="USN"
-              />
-              <div className="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
-            </div>
-            <div className="mb-6 form-password-toggle fv-plugins-icon-container fv-plugins-bootstrap5-row-valid">
-              <div className="input-group input-group-merge has-validation">
-                <input
-                  type="password"
-                  id="password"
-                  className="form-control form-control-lg"
-                  name="password"
-                  placeholder="Password"
-                  aria-describedby="password"
-                />
-                <span className="input-group-text cursor-pointer">
-                  <i className="bx bx-hide"></i>
-                </span>
-              </div>
-              <div className="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
-            </div>
-            <div className="mb-6">
-              <button className="btn btn-lg btn-primary d-grid w-100" type="submit">
-                Sign in
-              </button>
-            </div>
-            <div className="w-100 text-center">
-              <a href="#" className="fw-semibold">
-                Forgot Password?
-              </a>
-            </div>
-            <input type="hidden" />
-          </form>
+          <StudentLoginForm />
         </AuthCard>
         <CopyrightNotice />
-        <InitPasswordToggler/>
       </AuthLayout>
     </GuestOnlyGuard>
   );
